09dd16a9440f17f323e4f8fafb9545306c8b5e46
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2016-12-10 20:38:35 -0800

    Incorporating Objective C and adding AFNetworking

---
+++ bridge.h
@@ -0,0 +1,16 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Lisa Steele on 12/10/16.
+//  Copyright © 2016 Lisa Steele. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
+
+#import AFNetworking/AFNetworking.h
+
